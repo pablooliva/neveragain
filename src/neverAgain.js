36b723065f65fff3d491be545a54f4ca6f4a4b92
Pablo Oliva <p.o@pablooliva.de>
2020-04-20 00:00:32 +0200

    Remove orphaned files.

--- src/neverAgain.js
+++
@@ -1,7 +0,0 @@
-import * as Mark from 'mark.js';
-window.onload = function () {
-    window.alert('loaded');
-    var markInstance = new Mark(document.body);
-    markInstance.mark('Using');
-};
-//# sourceMappingURL=neverAgain.js.map
